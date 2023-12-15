275f6b26ea5fda0d4c9fbf3bd4b2c0b019406f45
git <raiane.rze@gmail.com>
2023-12-15 04:39:46 -0300

    corrects checkbox component

--- src/components/Checkbox/Checkbox.stories.ts
+++ src/components/Checkbox/Checkbox.stories.ts
@@ -3,10 +3,45 @@ import { action } from '@storybook/addon-actions';
 
 import Checkbox, { Input } from "./Checkbox.marko";
 
+const meta = {
+    title: "Checkbox",
+    component: Checkbox,
+    parameters: {
+        design: {
+            type: "figma",
+            url: "https://www.figma.com/file/vBQD0M0G6zZfxu5ctekUD8/Figma-Plugin-SDK-Design-System?type=design&node-id=148-8002&mode=design&t=ZTf0R45O5VWbPhrY-4",
+        },
+        status: {
+            type: "beta", // 'beta' | 'stable' | 'deprecated' | 'releaseCandidate'
+            url: "http://www.url.com/status", // will make the tag a link
+            // statuses: {
+            //   add custom statuses for this story here
+            // }
+        },
+    },
+} satisfies Meta<Input>;
+
 export default {
     title: "Checkbox",
     component: Checkbox,
     argTypes: {
+        variant: {
+            control: 'radio',
+            options: ['primary', 'secondary', 'tertiary'],
+            description: 'The variant of the checkbox'
+        },
+        disabled: {
+            control: 'boolean',
+            description: 'Whether the checkbox is disabled'
+        },
+        label: {
+            control: 'text',
+            description: 'The label of the checkbox'
+        },
+        uniqueId: {
+            control: 'text',
+            description: 'Unique ID for the checkbox'
+        },
         checked: {
             control: 'boolean',
             description: 'Whether the checkbox is checked'
@@ -15,25 +50,17 @@ export default {
             control: 'boolean',
             description: 'Whether the checkbox is in an indeterminate state'
         },
-        value: {
-            control: 'text',
-            description: 'The value of the checkbox'
-        },
-        disabled: {
-            control: 'boolean',
-            description: 'Whether the checkbox is disabled'
-        },
         tabindex: {
             control: { type: 'number', min: -1 },
             description: 'Tab index of the checkbox'
         },
         className: {
-            control: 'text',
+            control: 'click me',
             description: 'Custom class name for styling'
         },
-        uniqueId: {
+        renderBody: {
             control: 'text',
-            description: 'Unique ID for the checkbox'
+            description: 'Custom body content for the label'
         },
         onClick: { action: 'clicked' },
         onChange: { action: 'changed' },
@@ -46,13 +73,14 @@ type Story = StoryObj<Input>;
 
 export const Default: Story = {
     args: {
+        variant: "primary",
+        disabled: false,
         checked: false,
         mixed: false,
-        value: '',
-        disabled: false,
         tabindex: 0,
+        className: '',
         uniqueId: 'checkbox--default',
-        className: ''
+        label: 'Default Checkbox',
     },
     play: async ({ canvasElement }) => {
         const input = canvasElement.querySelector('input');
@@ -60,33 +88,5 @@ export const Default: Story = {
         input.addEventListener('change', action('changed'));
         input.addEventListener('focus', action('focused'));
         input.addEventListener('blur', action('blurred'));
-    }
-};
-
-export const Checked: Story = {
-    args: {
-        ...Default.args,
-        checked: true
-    }
-};
-
-export const Disabled: Story = {
-    args: {
-        ...Default.args,
-        disabled: true
-    }
-};
-
-export const CustomClass: Story = {
-    args: {
-        ...Default.args,
-        className: 'custom-checkbox'
-    }
-};
-
-export const Indeterminate: Story = {
-    args: {
-        ...Default.args,
-        mixed: true
     }
 };
