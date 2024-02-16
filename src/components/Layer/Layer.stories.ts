6b4751a5290ed78360b134b6b6fc0c6059fef77b
git <raiane.rze@gmail.com>
2024-02-15 21:00:32 -0300

    adds proper style

--- src/components/Layer/Layer.stories.ts
+++ src/components/Layer/Layer.stories.ts
@@ -1,19 +1,81 @@
-import { Meta, Story } from '@storybook/marko';
-import Layer from './Layer.marko';
+// LayerComponent.stories.ts
+import { Meta, StoryObj } from '@storybook/marko';
+import { action } from '@storybook/addon-actions';
+import LayerTree, { Type, State, type Layer, SampleValues } from './LayerTree.marko';
 
 export default {
     title: 'Layer',
-    component: Layer,
+    component: LayerTree,
     argTypes: {
-        layers: {
-            control: 'object',
-            description: 'The nested layers structure',
+        selectLayer: { action: 'selectLayer' },
+        toggleExpand: { action: 'toggleExpand' },
+        toggleLock: { action: 'toggleLock' },
+        toggleVisibility: { action: 'toggleVisibility' },
+    },
+    parameters: {
+        controls: {
+            expanded: true,
         },
     },
 } as Meta;
 
-const Template: Story = (args) => ({
-    input: args,
-});
+const Template: StoryObj = {
+    render: (args) => ({
+        template: (args),
+        methods: {
+            selectLayer: action('selectLayer'),
+            toggleExpand: action('toggleExpand'),
+            toggleLock: action('toggleLock'),
+            toggleVisibility: action('toggleVisibility'),
+        },
+    }),
+    args: {
+        ...SampleValues,
+    },
+};
 
-export const Default = Template.bind({});
+// export const Default = Template.bind({});
+
+export const LayerSelected = {
+    ...Template,
+    args: {
+        ...SampleValues,
+        rootLayers: SampleValues.map((layer, index) => ({
+            ...layer,
+            selected: index === 0, // Select the first layer
+        })),
+    },
+};
+
+export const LayerExpanded = {
+    ...Template,
+    args: {
+        ...SampleValues,
+        rootLayers: SampleValues.map((layer, index) => ({
+            ...layer,
+            expanded: index === 0, // Expand the first layer
+        })),
+    },
+};
+
+export const LayerLocked = {
+    ...Template,
+    args: {
+        ...SampleValues,
+        rootLayers: SampleValues.map((layer, index) => ({
+            ...layer,
+            locked: index === 0, // Lock the first layer
+        })),
+    },
+};
+
+export const LayerHidden = {
+    ...Template,
+    args: {
+        ...SampleValues,
+        rootLayers: SampleValues.map((layer, index) => ({
+            ...layer,
+            hidden: index === 0, // Hide the first layer
+        })),
+    },
+};
