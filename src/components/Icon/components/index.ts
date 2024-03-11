8da9a4f40b20f518314a2ee883da7fde2b38e18d
Alberto Nunes <bisneto.alberto@gmail.com>
2024-03-11 13:06:30 -0300

    Refactor Icon component into a mini-library to include multiple images

---
+++ src/components/Icon/components/index.ts
@@ -0,0 +1,176 @@
+//Icons
+import IconAdjust from "./adjust.marko";
+import IconAlert from "./alert.marko";
+import IconAngle from "./angle.marko";
+import IconArrowLeftRight from "./arrow-left-right.marko";
+import IconUpDown from "./arrow-up-down.marko";
+import IconAutoLayoutHorizontal from "./auto-layout-horizontal.marko";
+import IconAutoLayoutVertical from "./auto-layout-vertical.marko";
+import IconBack from "./back.marko";
+import IconBlendEmpty from "./blend-empty.marko";
+import IconBlend from "./blend.marko";
+import IconBreak from "./break.marko";
+import IconCaretDown from "./caret-down.marko";
+import IconCaretLeft from "./caret-left.marko";
+import IconCaretRight from "./caret-right.marko";
+import IconCaretUp from "./caret-up.marko";
+import IconCheck from "./check.marko";
+import IconClose from "./close.marko";
+import IconComponent from "./component.marko";
+import IconCornerRadius from "./corner-radius.marko";
+import IconCorners from "./corners.marko";
+import IconDistributeHorizontalSpacing from "./distribute-horizontal-spacing.marko";
+import IconDistributeVerticalSpacing from "./distribute-vertical-spacing.marko";
+import IconDraft from "./draft.marko";
+import IconEffects from "./effects.marko";
+import IconEllipses from "./ellipses.marko";
+import IconEyedropper from "./eyedropper.marko";
+import IconForward from "./forward.marko";
+import IconGroup from "./group.marko";
+import IconHidden from "./hidden.marko";
+import IconHorizontalPadding from "./horizontal-padding.marko";
+import IconHyperlink from "./hyperlink.marko";
+import IconImage from "./image.marko";
+import IconInstance from "./instance.marko";
+import IconKey from "./key.marko";
+import IconLayoutAlignBottom from "./layout-align-bottom.marko";
+import IconAlignHorizontalCenters from "./layout-align-horizontal-centers.marko";
+import IconAlignLeft from "./layout-align-left.marko";
+import IconAlignRight from "./layout-align-right.marko";
+import IconAlignTop from "./layout-align-top.marko";
+import IconAlignVerticalCenters from "./layout-align-vertical-centers.marko";
+import IconLayoutGridColumns from "./layout-grid-columns.marko";
+import IconLayoutGridRows from "./layout-grid-rows.marko";
+import IconLayoutGridUniform from "./layout-grid-uniform.marko";
+import IconLibrary from "./library.marko";
+import IconLinkBroken from "./link-broken.marko";
+import IconLinkConnected from "./link-connected.marko";
+import IconListDetailed from "./list-detailed.marko";
+import IconListTile from "./list-tile.marko";
+import IconList from "./list.marko";
+import IconLockOff from "./lock-off.marko";
+import IconLockOn from "./lock-on.marko";
+import IconMinus from "./minus.marko";
+import IconPlay from "./play.marko";
+import IconPlus from "./plus.marko";
+import IconRandom from "./random.marko";
+import IconRecent from "./recent.marko";
+import IconResizeToFit from "./resize-to-fit.marko";
+import IconResolveFilled from "./resolve-filled.marko";
+import IconResolve from "./resolve.marko";
+import IconReverse from "./reverse.marko";
+import IconSearchLarge from "./search-large.marko";
+import IconSearch from "./search.marko";
+import IconSettings from "./settings.marko";
+import IconShare from "./share.marko";
+import IconSmiley from "./smiley.marko";
+import IconSortAlphaAsc from "./sort-alpha-asc.marko";
+import IconSortAlphaDsc from "./sort-alpha-dsc.marko";
+import IconSortTopBottom from "./sort-top-bottom.marko";
+import IconSpacing from "./spacing.marko";
+import IconSpinner from "./spinner.marko";
+import IconStarOff from "./star-off.marko";
+import IconStarOn from "./star-on.marko";
+import IconStrokeWeight from "./stroke-weight.marko";
+import IconStyles from "./styles.marko";
+import IconSwap from "./swap.marko";
+import IconTheme from "./theme.marko";
+import IconTidyUpGrid from "./tidy-up-grid.marko";
+import IconTidyUpListHorizontal from "./tidy-up-list-horizontal.marko";
+import IconTidyUpListVertical from "./tidy-up-list-vertical.marko";
+import IconTimer from "./timer.marko";
+import IconFrame from "./top-level-frame.marko";
+import IconTrash from "./trash.marko";
+import IconVerticalPadding from "./vertical-padding.marko";
+import IconVisible from "./visible.marko";
+import IconWarningLarge from "./warning-large.marko";
+import IconWarning from "./warning.marko";
+
+export {
+  IconAdjust,
+  IconAlert,
+  IconAngle,
+  IconArrowLeftRight,
+  IconUpDown,
+  IconAutoLayoutHorizontal,
+  IconAutoLayoutVertical,
+  IconBack,
+  IconBlendEmpty,
+  IconBlend,
+  IconBreak,
+  IconCaretDown,
+  IconCaretLeft,
+  IconCaretRight,
+  IconCaretUp,
+  IconCheck,
+  IconClose,
+  IconComponent,
+  IconCornerRadius,
+  IconCorners,
+  IconDistributeHorizontalSpacing,
+  IconDistributeVerticalSpacing,
+  IconDraft,
+  IconEffects,
+  IconEllipses,
+  IconEyedropper,
+  IconForward,
+  IconFrame,
+  IconGroup,
+  IconHidden,
+  IconHorizontalPadding,
+  IconHyperlink,
+  IconImage,
+  IconInstance,
+  IconKey,
+  IconLayoutAlignBottom,
+  IconAlignHorizontalCenters,
+  IconAlignLeft,
+  IconAlignRight,
+  IconAlignTop,
+  IconAlignVerticalCenters,
+  IconLayoutGridColumns,
+  IconLayoutGridRows,
+  IconLayoutGridUniform,
+  IconLibrary,
+  IconLinkBroken,
+  IconLinkConnected,
+  IconListDetailed,
+  IconListTile,
+  IconList,
+  IconLockOff,
+  IconLockOn,
+  IconMinus,
+  IconPlay,
+  IconPlus,
+  IconRandom,
+  IconRecent,
+  IconResizeToFit,
+  IconResolveFilled,
+  IconResolve,
+  IconReverse,
+  IconSearchLarge,
+  IconSearch,
+  IconSettings,
+  IconShare,
+  IconSmiley,
+  IconSortAlphaAsc,
+  IconSortAlphaDsc,
+  IconSortTopBottom,
+  IconSpacing,
+  IconSpinner,
+  IconStarOff,
+  IconStarOn,
+  IconStrokeWeight,
+  IconStyles,
+  IconSwap,
+  IconTheme,
+  IconTidyUpGrid,
+  IconTidyUpListHorizontal,
+  IconTidyUpListVertical,
+  IconTimer,
+  IconTrash,
+  IconVerticalPadding,
+  IconVisible,
+  IconWarningLarge,
+  IconWarning,
+};
